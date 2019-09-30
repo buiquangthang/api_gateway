df2c7805ed21fbec0739e998e5dba09e389bc1c1
bui.quang.thang <SUN-ASTERISK\bui.quang.thang@b121188-pc.sun-asterisk.com>
2019-09-30 17:43:51 +0700

    Add check authen controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post   'user_token'      => 'user_token#create'
+
+  # Home controller routes.
+  root   'home#index'
+  get    'auth'            => 'home#auth'
 end
